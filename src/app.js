62a17793914bae28168347a67be4d4f851cfc72d
MaguiSanchez <162079957+MaguiSanchez@users.noreply.github.com>
2024-08-31 04:54:51 +0000

    bubble :(

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ window.onload = function() {
   let sym = ["♦", "♥", "♠", "♣"];
   let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let cards = [];
+  let arrCards = [];
   let draw = document.getElementById("draw");
   let sort = document.getElementById("sort");
   let numCards = document.getElementById("numCards");
@@ -26,38 +27,39 @@ window.onload = function() {
     } else {
       color = "red";
     }
-
     let cardDiv = document.createElement("div");
     cardDiv.style.border = "1px solid black";
     cardDiv.style.borderRadius = "4px";
     cardDiv.style.width = "70px";
-    cardDiv.style.height = "100px";
+    cardDiv.style.height = "95px";
     cardDiv.style.margin = "5px";
-    cardDiv.style.overflow = "auto";
+    // cardDiv.style.overflow = "auto";
     cardDiv.style.paddingLeft = "2px";
     cardDiv.style.paddingRight = "2px";
     cardDiv.style.backgroundColor = "white";
+    cardDiv.style.display = "flex";
+    cardDiv.style.justifyContent = "space-between";
 
     let cardSymDivTop = document.createElement("div");
     cardSymDivTop.innerHTML = randomCardSym;
     cardSymDivTop.style.color = color;
-    cardSymDivTop.style.position = "left";
+    // cardSymDivTop.style.position = "left";
 
     let cardNumDiv = document.createElement("div");
     cardNumDiv.innerHTML = randomCardNum;
     cardNumDiv.style.fontSize = "25px";
-    cardNumDiv.style.padding = "3px";
-    cardNumDiv.style.textAlign = "center";
+    cardNumDiv.style.alignContent = "center";
 
     let cardSymDivBottom = document.createElement("div");
     cardSymDivBottom.innerHTML = randomCardSym;
     cardSymDivBottom.style.color = color;
-    cardSymDivTop.style.position = "rigth";
+    // cardSymDivBottom.style.position = "rigth";
     cardSymDivBottom.style.transform = "rotate(180deg)";
 
     cardDiv.appendChild(cardSymDivTop);
     cardDiv.appendChild(cardNumDiv);
     cardDiv.appendChild(cardSymDivBottom);
+    arrCards.push(randomCardNum);
 
     // cardDiv.dataset.num = randomCardNum;
     return cardDiv;
@@ -71,20 +73,34 @@ window.onload = function() {
       cards.push(cardRandom);
       cardsContainer.appendChild(cardRandom);
     }
+    console.log(cards);
+    console.log(arrCards);
   });
-
   // SORT
   const bubbleSort = arr => {
-    sortContainer.innerHTML = "";
+    //[8, 10, 4, 1, 3]
+    //              4
+    // sortContainer.innerHTML = "";
     let wall = arr.length - 1; //iniciamos el wall o muro al final del array
+    //      4>0
     while (wall > 0) {
+      //true
+      //espacio de memoria donde guardamos un valor
       let index = 0;
+      //comparacion: i=0 < 4 // true entra al if
       while (index < wall) {
         //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
-        if (arr[index][0] > arr[index + 1][0]) {
+        // posicion 0: 8 > posicion 0+1: 10 //true? ño  // sgte iteracion=> 1:10 > 1+1:4 posiscion 2
+        if (arr[index] > arr[index + 1]) {
           //intercambio de cartas
+          //espacio de memoria
+          // lee la posicion 1
+          //  10
           let aux = arr[index];
+          // i 1: 10        2:4
           arr[index] = arr[index + 1];
+          //posicion 2 --> asignamos aux q valia 10
+          // [8, 4, 10, 1, 3]
           arr[index + 1] = aux;
           // for (let j = 0; j < arr.length; j++) {
           //   let randCardNum = arr[j][0];
@@ -100,14 +116,19 @@ window.onload = function() {
           //   sortContainer.appendChild(sortDiv);
           // }
         }
+        //index=0--> +1 ahora vale index=1, pasado el if ..+1 tons i=2
         index++;
       }
+      //wall valia 4 --menos1 es igual a 3
       wall--; //disminuir la pared para optimizar
     }
     return arr;
   };
 
+  // let array = [8, 10, 4, 1, 3];
+  // console.log(bubbleSort(array));
+
   sort.addEventListener("click", () => {
-    bubbleSort(cards);
+    console.log(bubbleSort(arrCards));
   });
 };
