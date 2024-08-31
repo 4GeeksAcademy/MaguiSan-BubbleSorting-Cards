431eb7fce667802eb90ccf0301407ea9876255b2
MaguiSanchez <162079957+MaguiSanchez@users.noreply.github.com>
2024-08-31 07:22:55 +0000

    the last :(

--- src/app.js
+++ src/app.js
@@ -5,6 +5,21 @@ window.onload = function() {
   // declaracion de variables
   let sym = ["♦", "♥", "♠", "♣"];
   let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let numValue = {
+    A: 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    J: 11,
+    Q: 12,
+    K: 13
+  };
   let cards = [];
   let arrCards = [];
   let draw = document.getElementById("draw");
@@ -33,7 +48,6 @@ window.onload = function() {
     cardDiv.style.width = "70px";
     cardDiv.style.height = "95px";
     cardDiv.style.margin = "5px";
-    // cardDiv.style.overflow = "auto";
     cardDiv.style.paddingLeft = "2px";
     cardDiv.style.paddingRight = "2px";
     cardDiv.style.backgroundColor = "white";
@@ -43,7 +57,6 @@ window.onload = function() {
     let cardSymDivTop = document.createElement("div");
     cardSymDivTop.innerHTML = randomCardSym;
     cardSymDivTop.style.color = color;
-    // cardSymDivTop.style.position = "left";
 
     let cardNumDiv = document.createElement("div");
     cardNumDiv.innerHTML = randomCardNum;
@@ -53,82 +66,75 @@ window.onload = function() {
     let cardSymDivBottom = document.createElement("div");
     cardSymDivBottom.innerHTML = randomCardSym;
     cardSymDivBottom.style.color = color;
-    // cardSymDivBottom.style.position = "rigth";
     cardSymDivBottom.style.transform = "rotate(180deg)";
 
     cardDiv.appendChild(cardSymDivTop);
     cardDiv.appendChild(cardNumDiv);
     cardDiv.appendChild(cardSymDivBottom);
-    arrCards.push(randomCardNum);
-
-    // cardDiv.dataset.num = randomCardNum;
     return cardDiv;
   }
   //generador de un array de cartas random
   draw.addEventListener("click", () => {
     cardsContainer.innerHTML = "";
-    cards = [];
+    arrCards = [];
     for (let i = 0; i < numCards.value; i++) {
       let cardRandom = drawCard();
-      cards.push(cardRandom);
+      arrCards.push(cardRandom);
       cardsContainer.appendChild(cardRandom);
+      // arrCards.push([cardValue, cardDiv]);
     }
-    console.log(cards);
-    console.log(arrCards);
   });
   // SORT
   const bubbleSort = arr => {
-    //[8, 10, 4, 1, 3]
-    //              4
-    // sortContainer.innerHTML = "";
-    let wall = arr.length - 1; //iniciamos el wall o muro al final del array
-    //      4>0
-    while (wall > 0) {
-      //true
-      //espacio de memoria donde guardamos un valor
-      let index = 0;
-      //comparacion: i=0 < 4 // true entra al if
-      while (index < wall) {
-        //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
-        // posicion 0: 8 > posicion 0+1: 10 //true? ño  // sgte iteracion=> 1:10 > 1+1:4 posiscion 2
-        if (arr[index] > arr[index + 1]) {
-          //intercambio de cartas
-          //espacio de memoria
-          // lee la posicion 1
-          //  10
-          let aux = arr[index];
-          // i 1: 10        2:4
-          arr[index] = arr[index + 1];
-          //posicion 2 --> asignamos aux q valia 10
-          // [8, 4, 10, 1, 3]
-          arr[index + 1] = aux;
-          // for (let j = 0; j < arr.length; j++) {
-          //   let randCardNum = arr[j][0];
-          //   let randCardSym = arr[j][1];
-          //   let sortDiv = document.createElement("div");
-          //   sortDiv.style.display = "flex";
-          //   sortDiv.style.alignItems = "center";
-          //   let indCounter = document.createElement("div");
-          //   indCounter.innerHTML = index;
-          //   indCounter.style.fontSize = "10px";
+    let wall = arr.length - 1; //iniciamos el muro al final del array
 
-          //   sortDiv.appendChild(indCounter);
-          //   sortContainer.appendChild(sortDiv);
-          // }
+    let lineDiv = document.createElement("div");
+    lineDiv.style.marginTop = "10px";
+    lineDiv.style.display = "flex";
+    lineDiv.style.alignItems = "center";
+    sortContainer.innerHTML = "";
+    sortContainer.appendChild(lineDiv);
+    while (wall > 0) {
+      let index = 0;
+      let otherDiv = document.createElement("div");
+      otherDiv.style.display = "flex";
+      while (index < wall) {
+        if (arr[index][0] > arr[index + 1][0]) {
+          [arr[index], arr[index + 1]] = [arr[index + 1], arr[index]];
+          otherDiv.innerHTML = "";
+          arr.forEach(item => {
+            otherDiv.innerHTML += item[1];
+          });
+          lineDiv.appendChild(otherDiv);
+          otherDiv = document.createElement("div");
+          otherDiv.style.display = "flex";
         }
-        //index=0--> +1 ahora vale index=1, pasado el if ..+1 tons i=2
         index++;
       }
-      //wall valia 4 --menos1 es igual a 3
       wall--; //disminuir la pared para optimizar
     }
+
+    let lastDiv = document.createElement("div");
+    lastDiv.style.display = "flex";
+    arr.forEach(item => {
+      lastDiv.innerHTML += item[1];
+    });
+    lineDiv.appendChild(lastDiv);
+
     return arr;
   };
 
-  // let array = [8, 10, 4, 1, 3];
-  // console.log(bubbleSort(array));
-
   sort.addEventListener("click", () => {
-    console.log(bubbleSort(arrCards));
+    // console.log(arrCards);
+    let sortDiv = document.createElement("div");
+    sortDiv.style.display = "flex";
+    sortDiv.style.alignItems = "center";
+
+    sortContainer.innerHTML = "";
+    sortContainer.appendChild(sortDiv);
+    arrCards.forEach(item => {
+      sortDiv.innerHTML += item[1];
+    });
+    bubbleSort(arrCards);
   });
 };
